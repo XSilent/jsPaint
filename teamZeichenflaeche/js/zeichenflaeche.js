0c993f4a322c45fc9e9367be7d62f3e8c2f1f12e
NoMuellerCode <ferry.kluger@gmx.de>
2013-07-04 09:30:29 +0200

    Initialer Commit von teamZeichenflaeche.

---
+++ teamZeichenflaeche/js/zeichenflaeche.js
@@ -0,0 +1,16 @@
+alert("Hallo Wurst!");
+
+function myCanvas () {
+
+	var canvasObject;
+	var history;
+	var dimensions = {
+		width: '400';
+		height: '300';
+	}
+
+	function setCanvas(element) {
+		this.canvasObject = element;
+	}
+	
+}
