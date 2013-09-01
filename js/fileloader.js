ffece220895b20707c8d2cd6dc00f4cbbabb4718
XSilent <michael.neuhaus@gmx.de>
2013-09-01 14:58:28 +0200

    New feature: Load images from local filesystem (to be placed on canvas)

---
+++ js/fileloader.js
@@ -0,0 +1,64 @@
+/**
+ * FileLoader - for accessing images
+ * from local filesystem
+ *
+ * @param canvasHandler
+ * @param fileSelectorId
+ * @constructor
+ */
+var FileLoader = function(canvasHandler, fileSelectorId, resizeCanvas)
+{
+	var _canvasHandler = canvasHandler;
+	var _fileSelectorId = fileSelectorId;
+	var _resizeCanvas = resizeCanvas;
+
+	/**
+	 *
+	 * @param event
+	 */
+	this.handleFileSelect = function(event)
+	{
+		var files = event.target.files; // get FileList object
+
+		try {
+			var image = new Image();
+
+			var reader = new FileReader();
+
+			// asynchronous loading with anonymouse
+			// function, assigned to the onload event
+			reader.onload = (function(aImg) { return function(e)
+			{
+				aImg.src = e.target.result;
+
+				var canvas = _canvasHandler.getContext('2d');
+
+				if (_resizeCanvas) {
+					canvas.canvas.height = image.height;
+					canvas.canvas.width = image.width;
+				}
+
+				var destX = canvas.canvas.width;
+				var destY = canvas.canvas.height;
+
+				if (image.width < destX) {
+					destX = image.width;
+				}
+
+				if (image.height < destY) {
+					destY   = image.height;
+				}
+
+				canvas.drawImage(image, 0, 0, destX, destY);
+			}; })(image);
+
+			reader.readAsDataURL(files[0]);
+
+		} catch(e) {
+			alert('Some error occurred: Could not open select file');
+		}
+	};
+
+	// register event listener on file selector
+	document.getElementById(_fileSelectorId).addEventListener('change', this.handleFileSelect, false);
+};
